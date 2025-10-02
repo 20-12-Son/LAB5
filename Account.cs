a5c5b92b25e207e32e18d8f3d259d84721229ef6
20-12-Son <anhcanteam2012@gmail.com>
2025-10-02 20:54:52 +0700

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LAB5.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Họ tên bắt buộc nhập")]
+        [StringLength(50, ErrorMessage = "Họ tên tối đa 50 ký tự")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email bắt buộc nhập")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Số điện thoại bắt buộc nhập")]
+        [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
+        public string Phone { get; set; }
+
+        [StringLength(100, ErrorMessage = "Địa chỉ tối đa 100 ký tự")]
+        public string Address { get; set; }
+
+        [DataType(DataType.ImageUrl, ErrorMessage = "Link ảnh không hợp lệ")]
+        public string Avatar { get; set; }
+
+        [Required(ErrorMessage = "Ngày sinh bắt buộc nhập")]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        [Required(ErrorMessage = "Giới tính bắt buộc nhập")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu bắt buộc nhập")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Mật khẩu từ 6-20 ký tự")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Url(ErrorMessage = "Địa chỉ Facebook không hợp lệ")]
+        public string Facebook { get; set; }
+    }
+}
---
+++ AccountsControllers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LAB5.Models;
+
+namespace LAB5.Controllers
+{
+    public class AccountsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Account acc)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Success", acc);
+            }
+            return View(acc);
+        }
+    }
+}
